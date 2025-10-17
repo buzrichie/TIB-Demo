d0b39cfb7e2b04612b9a28e91eee376b559e4774
Richmond Nyarko <richmondnyarko123@gmail.com>
2025-10-17 14:29:19 +0000

    perf update feature to use ApiResponse structure

--- src/main/java/com/amalitech/tib/security/AppAccessDeniedHandler.java
+++ src/main/java/com/amalitech/tib/security/AppAccessDeniedHandler.java
@@ -5,28 +5,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
 
 @Component
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class AppAccessDeniedHandler implements AccessDeniedHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
 
         ApiResponse<String> errorResponse = ApiResponse.error(
-                authException.getMessage()
+                accessDeniedException.getMessage()
         );
 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
         response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
+
